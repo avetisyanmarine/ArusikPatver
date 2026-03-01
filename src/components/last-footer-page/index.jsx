704baad9f03a8191fe16aa67223f4babe7f58c92
avetisyanmarine <marineavet2006@gmail.com>
2026-03-01 23:25:39 +0400

    Initial commit for WeddingSite

--- src/components/last-footer-page/index.jsx
+++ src/components/last-footer-page/index.jsx
@@ -1,38 +1,42 @@
-import { Container } from "../../GlobalStyle"
-import { LastFooterPagePart, LastFooterPagePartContext } from "./styled"
-import PhoneSvg from "../../assets/vectors/Phone.svg"
+import { Container } from "../../GlobalStyle";
+import { LastFooterPagePart, LastFooterPagePartContext } from "./styled";
+import PhoneSvg from "../../assets/vectors/Phone.svg";
 
 export const LastFooterPage = () => {
-    return (
-        <LastFooterPagePart>
-            <Container>
-                <LastFooterPagePartContext>
-                    <div>
-                        <div>
-                            <div>
-                                <p>
-                                    Փեսա
-                                </p>
-                                <p>
-                                    Հարս
-                                </p>
-                            </div>
-                        </div>
-                        <div>
-                            <img loading="lazy" src={PhoneSvg} />
-                            <div>
-                                <p> Սեղմիր՝
-                                    <a href="tel:077215009"> 077-21-50-09</a>
-                                </p>
-                                <p> Սեղմիր՝
-                                    <a href="tel:077149983"> 077-14-99-83</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <p>Պատրաստվել է ՀարսնաՔրոջ կողմից</p>
-                </LastFooterPagePartContext>
-            </Container>
-        </LastFooterPagePart>
-    )
-}
+  return (
+    <LastFooterPagePart>
+      <Container>
+        <LastFooterPagePartContext>
+          <div>
+            <div>
+              <div>
+                <p>Փեսա</p>
+                <p>Հարս</p>
+              </div>
+            </div>
+            <div>
+              <img loading="lazy" src={PhoneSvg} />
+              <div>
+                <p>
+                  {" "}
+                  Սեղմիր՝
+                  <a href="tel:077215009"> 077-21-50-09</a>
+                </p>
+                <p>
+                  {" "}
+                  Սեղմիր՝
+                  <a href="tel:077149983"> 077-14-99-83</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <p>
+            Պատրաստվել է
+            <a href="https://www.instagram.com/webinvite.am/"> WEB INVITE </a>
+            կողմից
+          </p>{" "}
+        </LastFooterPagePartContext>
+      </Container>
+    </LastFooterPagePart>
+  );
+};
