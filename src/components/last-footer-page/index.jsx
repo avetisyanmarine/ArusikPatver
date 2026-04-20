f6c7dd29cef2217051d7d47bf438da170eba8cf1
avetisyanmarine <marineavet2006@gmail.com>
2026-04-20 22:05:51 +0400

    qwe

--- src/components/last-footer-page/index.jsx
+++ src/components/last-footer-page/index.jsx
@@ -9,17 +9,12 @@ export const LastFooterPage = () => {
       <Container>
         <LastFooterPagePartContext>
           <div className="text-center flex flex-col items-center gap-4">
-            <p>
-              Պատրաստվել է
-              <div className="flex items-center gap-1 mx-1">
-                <img src={Instagram} alt="Instagram" className="inline-block" />
-                <a href="https://www.instagram.com/webinvite.am/">
-                  {" "}
-                  WEB INVITE{" "}
-                </a>
-              </div>
-              կողմից
-            </p>
+            <p>Պատրաստվել է</p>
+            <div className="flex items-center gap-1 mx-1">
+              <img src={Instagram} alt="Instagram" className="inline-block" />
+              <a href="https://www.instagram.com/webinvite.am/"> WEB INVITE </a>
+            </div>
+            <p>կողմից</p>
           </div>
         </LastFooterPagePartContext>
       </Container>
